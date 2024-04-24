9a2830636135ac759c048eb64e1f698ad353922e
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-04-24 16:05:36 +0300

    initial commit

---
+++ src/main/java/com/nadola/JobApplication/job/JobController.java
@@ -0,0 +1,19 @@
+package com.nadola.JobApplication.job;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+
+    //NOT CONNECTED TO A DATABASE SO WE SHALL USE AN ARRAY INSTEAD TO MANAGE JOBS
+    private List<Job> jobs = new ArrayList<>();
+
+    @GetMapping("/jobs")
+    public List<Job> findAll(){
+        return jobs;
+    }
+}
