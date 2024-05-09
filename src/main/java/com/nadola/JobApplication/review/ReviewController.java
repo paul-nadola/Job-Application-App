6965cbec80dc91e0360b359cb0f9b550c9c7e773
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-05-09 15:50:04 +0300

    added put functionality

--- src/main/java/com/nadola/JobApplication/review/ReviewController.java
+++ src/main/java/com/nadola/JobApplication/review/ReviewController.java
@@ -36,4 +36,18 @@ public class ReviewController {
                                                  @PathVariable Long reviewId) {
         return new ResponseEntity<>(reviewService.getReview(companyId, reviewId), HttpStatus.OK);
     }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(
+            @PathVariable Long companyId,
+            @PathVariable Long reviewId,
+            @RequestBody Review review
+    ) {
+        boolean isReviewUpdated = reviewService.updateReview(companyId, reviewId, review);
+        if (isReviewUpdated)
+            return new ResponseEntity<>("Review Updated Successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review Not Updated Successfully", HttpStatus.NOT_FOUND);
+    }
 }
+
--- src/main/java/com/nadola/JobApplication/review/ReviewService.java
+++ src/main/java/com/nadola/JobApplication/review/ReviewService.java
@@ -7,4 +7,5 @@ public interface ReviewService {
     List<Review> getAllReviews(Long companyId);
     boolean addReview(Long companyId, Review review);
     Review getReview(Long companyId, Long reviewId);
+    boolean updateReview (Long companyId, Long reviewId, Review review);
 }
