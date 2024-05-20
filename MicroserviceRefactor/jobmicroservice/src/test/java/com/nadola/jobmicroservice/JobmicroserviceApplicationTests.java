8cf9bb20972cdc2ae5da6c5f8fd313eb7323d9f8
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-05-20 16:29:41 +0300

    started microservices refactor

---
+++ MicroserviceRefactor/jobmicroservice/src/test/java/com/nadola/jobmicroservice/JobmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadola.jobmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
