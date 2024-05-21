dcb8dd3bba50b7f3cccb4f87d27694b77326b60c
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-05-21 10:07:43 +0300

    added review classess and intefaces

---
+++ MicroserviceRefactor/ReviewMicroservice/src/main/java/com/nadola/ReviewMicroservice/review/Review.java
@@ -0,0 +1,63 @@
+package com.nadola.ReviewMicroservice.review;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nadola.JobApplication.company.Company;
+import jakarta.persistence.*;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String rating;
+
+    @JsonIgnore
+    @ManyToOne
+    private Company company;
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
---
+++ MicroserviceRefactor/ReviewMicroservice/src/main/java/com/nadola/ReviewMicroservice/review/impl/ReviewServiceImpl.java
@@ -0,0 +1,90 @@
+package com.nadola.ReviewMicroservice.review.impl;
+
+import com.nadola.JobApplication.company.Company;
+import com.nadola.JobApplication.company.CompanyService;
+import com.nadola.JobApplication.review.Review;
+import com.nadola.JobApplication.review.ReviewRepository;
+import com.nadola.JobApplication.review.ReviewService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    private final CompanyService companyService;
+
+
+
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
+        this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if (company != null) {
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        //get all reviews then iterate with companyId
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream()
+                .filter(review -> review.getId().equals(reviewId))
+                .findFirst()
+                .orElse(null)
+                ;
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updatedReview) {
+        if (companyService.getCompanyById(companyId) != null){
+            updatedReview.setCompany(companyService.getCompanyById(companyId));
+            updatedReview.setId(reviewId);
+            reviewRepository.save(updatedReview);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        if (companyService.getCompanyById(companyId) != null && reviewRepository.existsById(reviewId)) {
+            //retrieve the review object first
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            //retrieve the company object
+            Company company = review.getCompany();
+
+            company.getReviews().remove(review);
+            review.setCompany(null);
+            //necessary because it is a bidirectional mapping
+            companyService.updateCompany(company, companyId);
+
+            reviewRepository.deleteById(reviewId);
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+}
